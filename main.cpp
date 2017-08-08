2e9985099033089bea26768a2dad5a7234aa435c
Alexander <alexander.lingtorp@gmail.com>
2017-08-08 05:35:35 -0700

    C++ instead of C, basic GUI layout done.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 /*
- * sv_subscriber_example.c
  *
  * Example program for Sampled Values (SV) subscriber
  *
@@ -20,11 +19,15 @@
 #include "../../nuklear.h"
 #include "nuklear_sdl_gl3.h"
 
-#define WINDOW_WIDTH 800
+#define WINDOW_WIDTH 400
 #define WINDOW_HEIGHT 600
 
 #define MAX_VERTEX_MEMORY 512 * 1024
 #define MAX_ELEMENT_MEMORY 128 * 1024
+
+#define APP_NAME "IEC61850-Server"
+#define FLOAT_MAX 99.99f
+#define FLOAT_MIN -FLOAT_MAX
 
 #include <signal.h>
 #include <stdio.h>
@@ -55,7 +58,7 @@ int main(int argc, char** argv) {
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    win = SDL_CreateWindow("Demo",
+    win = SDL_CreateWindow(APP_NAME,
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL|SDL_WINDOW_SHOWN|SDL_WINDOW_ALLOW_HIGHDPI);
     glContext = SDL_GL_CreateContext(win);
@@ -112,41 +115,37 @@ int main(int argc, char** argv) {
       nk_input_end(ctx);
 
       /* GUI */
-      if (nk_begin(ctx, "Demo", nk_rect(50, 50, 200, 200),
-          NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_SCALABLE|
-          NK_WINDOW_CLOSABLE|NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE))
-      {
+      if (nk_begin(ctx, APP_NAME, nk_rect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT),
+          NK_WINDOW_BORDER|NK_WINDOW_TITLE)) {
           nk_menubar_begin(ctx);
-          nk_layout_row_begin(ctx, NK_STATIC, 25, 2);
+          nk_layout_row_begin(ctx, NK_STATIC, 25, 1);
           nk_layout_row_push(ctx, 45);
-          if (nk_menu_begin_label(ctx, "FILE", NK_TEXT_LEFT, nk_vec2(120, 200))) {
+          if (nk_menu_begin_label(ctx, "PROPERTIES", NK_TEXT_LEFT, nk_vec2(120, 200))) {
               nk_layout_row_dynamic(ctx, 30, 1);
-              nk_menu_item_label(ctx, "OPEN", NK_TEXT_LEFT);
-              nk_menu_item_label(ctx, "CLOSE", NK_TEXT_LEFT);
-              nk_menu_end(ctx);
-          }
-          nk_layout_row_push(ctx, 45);
-          if (nk_menu_begin_label(ctx, "EDIT", NK_TEXT_LEFT, nk_vec2(120, 200))) {
-              nk_layout_row_dynamic(ctx, 30, 1);
-              nk_menu_item_label(ctx, "COPY", NK_TEXT_LEFT);
-              nk_menu_item_label(ctx, "CUT", NK_TEXT_LEFT);
-              nk_menu_item_label(ctx, "PASTE", NK_TEXT_LEFT);
+              nk_menu_item_label(ctx, "REMOVE ALL", NK_TEXT_LEFT);
+              nk_menu_item_label(ctx, "CLEAR ALL", NK_TEXT_LEFT);
               nk_menu_end(ctx);
           }
           nk_layout_row_end(ctx);
           nk_menubar_end(ctx);
 
-          enum {EASY, HARD};
-          static int op = EASY;
-          static int property = 20;
-          nk_layout_row_static(ctx, 30, 80, 1);
-          if (nk_button_label(ctx, "button"))
-              fprintf(stdout, "button pressed\n");
-          nk_layout_row_dynamic(ctx, 30, 2);
-          if (nk_option_label(ctx, "easy", op == EASY)) op = EASY;
-          if (nk_option_label(ctx, "hard", op == HARD)) op = HARD;
+          /* Property pane */
+          nk_layout_row_dynamic(ctx, 25, 2);
+          nk_label(ctx, "Property #1", NK_TEXT_LEFT);
+          nk_button_label(ctx, "Remove");
+
+          nk_layout_row_dynamic(ctx, 25, 2);
+          nk_label(ctx, "Name: ", NK_TEXT_LEFT);
+          static char text[32];
+          static int text_len = 0;
+          nk_edit_string(ctx, NK_EDIT_SIMPLE, text, &text_len, sizeof(text), nk_filter_default);
+
           nk_layout_row_dynamic(ctx, 25, 1);
-          nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1);
+          static float value = 0.0f;
+          nk_property_float(ctx, "Value:", 0.0f, &value, 100.0f, 0.1f, 1.0f);
+
+          nk_layout_row_dynamic(ctx, 50, 1);
+          nk_button_label(ctx, "+");
       }
       nk_end(ctx);
 
